e84631660c0cc754c3ca6cbe69a2dc4b58407eb8
Wanderley <100Palavr@gmail.com>
2022-01-23 15:33:20 -0300

    Update cubo.h

--- cubo.h
+++ cubo.h
@@ -8,54 +8,6 @@
 using namespace std;
 using namespace Eigen;
 
-// rotaciona em função do eixo Z
-Matrix4f rotationZpt(float angle) {
-	Matrix4f a = Matrix4f::Identity(4, 4);
-	a(0, 0) = cos(angle);
-	a(0, 1) = -sin(angle);
-	a(1, 0) = sin(angle);
-	a(1, 1) = cos(angle);
-	return a;
-}
-
-// rotaciona em função do eixo Y
-Matrix4f rotationYpt(float angle) {
-	Matrix4f a = Matrix4f::Identity(4, 4);
-	a(0, 0) = cos(angle);
-	a(0, 2) = sin(angle);
-	a(2, 0) = -sin(angle);
-	a(2, 2) = cos(angle);
-	return a;
-}
-
-// rotaciona em função do eixo X
-Matrix4f rotationXpt(float angle) {
-	Matrix4f a = Matrix4f::Identity(4, 4);
-	a(1, 1) = cos(angle);
-	a(1, 2) = -sin(angle);
-	a(2, 2) = cos(angle);
-	a(2, 1) = sin(angle);
-	return a;
-}
-
-// translarda
-Matrix4f translationpt(float tx, float ty, float tz) {
-	Matrix4f a = Matrix4f::Identity(4, 4);
-	a(0, 3) = tx;
-	a(1, 3) = ty;
-	a(2, 3) = tz;
-	return a;
-}
-
-// Gera uma matriz pra escalar
-Matrix4f scallingpt(float fx, float fy, float fz) {
-	Matrix4f a = Matrix4f::Identity(4, 4);
-	a(0, 0) = fx;
-	a(1, 1) = fy;
-	a(2, 2) = fz;
-	return a;
-}
-
 class Cubo {
 public:
 	Cubo() {
